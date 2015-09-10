cde0192a3a453115686323650a7c89668946cb8f
ssjjsj <794539236@qq.com>
2015-09-10 16:55:19 +0800

    program.cs

--- softRender/Program.cs
+++ softRender/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SlimDX;
 
 namespace softRender
 {
@@ -13,21 +14,40 @@ namespace softRender
             Surface s = new ImageSurface(300, 300);
             Buffer b = new Buffer(300, 300);
             Rasterization r = new Rasterization();
-            Vertex[] vertexList = new Vertex[3];
-            
-            vertexList[0] = new Vertex();
-            vertexList[0].pos = new SlimDX.Vector4(0, 300, 0, 0);
-            vertexList[0].color = new SlimDX.Color4(1.0f, 1.0f, 0.0f, 0.0f);
+            Vertex[] tringles = new Vertex[3];
 
-            vertexList[1] = new Vertex();
-            vertexList[1].pos = new SlimDX.Vector4(300, 300, 0, 0);
-            vertexList[1].color = new SlimDX.Color4(1.0f, 0.0f, 1.0f, 0.0f);
+            tringles[0] = new Vertex();
+            tringles[0].pos = new SlimDX.Vector4(-1, 1, -1, 1);
+            tringles[0].color = new SlimDX.Color4(1.0f, 1.0f, 0.0f, 0.0f);
 
-            vertexList[2] = new Vertex();
-            vertexList[2].pos = new SlimDX.Vector4(150, 0, 0, 0);
-            vertexList[2].color = new SlimDX.Color4(1.0f, 0.0f, 0.0f, 1.0f);
+            tringles[1] = new Vertex();
+            tringles[1].pos = new SlimDX.Vector4(1, 1, -1, 1);
+            tringles[1].color = new SlimDX.Color4(1.0f, 0.0f, 1.0f, 0.0f);
 
-            r.drawTriange(vertexList, b);
+            tringles[2] = new Vertex();
+            tringles[2].pos = new SlimDX.Vector4(0, -1, -1, 1);
+            tringles[2].color = new SlimDX.Color4(1.0f, 0.0f, 0.0f, 1.0f);
+
+            Camera c = new Camera(30f, 1, 1, 100, 300, 300);
+
+            //Cube cube = new Cube(new Vector4(-1f, -1f, -1f, 1f), new Vector4(1.0f, 1.0f, 1.0f, 1.0f));
+            //Matrix m = new Matrix();
+            //Matrix.RotationX(30.0f, out m);
+            //Matrix.Translation(0.0f, 0.0f, 5.0f);
+
+            //List<Vertex[]> vertexs = cube.getVertex();
+
+            //foreach (Vertex[] tringles in vertexList)
+            //{
+                for (int i=0; i<3; i++)
+                {
+                    tringles[i].pos = Vector4.Transform(tringles[i].pos, c.getClipMatrix());
+                    tringles[i].pos = new Vector4(tringles[i].pos.X / tringles[i].pos.W, tringles[i].pos.Y / tringles[i].pos.W, tringles[i].pos.Z / tringles[i].pos.W, 1.0f);
+                    tringles[i].pos = Vector4.Transform(tringles[i].pos,  c.getClipToScreenMatrix());
+                }
+
+                r.drawTriange(tringles, b);
+            //}
             s.Present(b);
         }
     }
